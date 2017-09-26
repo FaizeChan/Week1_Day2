3f6039d76f4adf54141025bc7464d688ef3aeef3
FaizeChan <obey.faize@gmail.com>
2017-09-26 14:22:15 -0400

    Practice with data types

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Faize!");
+
+            int age = 29;
+
+            Console.WriteLine("My age is " + age);
+
+            int favoriteNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("My favorite number is " + favoriteNumber+".");
+        }
+    }
+}
